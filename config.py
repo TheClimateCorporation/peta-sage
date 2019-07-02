8d6b7c754a351819e50c3ce4026634669793299c
Steven De Gryze <sdegryze@climate.com>
2019-07-02 12:38:45 -0700

    Renames

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 DATASET_URL = 's3://com.climate.production.analytics/dsw/scratch/sagemaker/sdegryze/mnist/'
+MODEL_URL_PREFIX = 's3://com.climate.production.analytics/dsw/scratch/sagemaker/models/peta_sage_hack/'
 IMAGE_NAME = '747953286079.dkr.ecr.us-east-1.amazonaws.com/data-analytics/petastorm-sagemaker'
-IAM_ROLE = "arn:aws:iam::335808120377:role/sagemaker-training-role"
+IAM_ROLE = 'arn:aws:iam::335808120377:role/sagemaker-training-role'
